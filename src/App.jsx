b4054a92e43d20bcc29cf427c3cf031bd10c9f51
SesanDaniel <obisesan.seyid@gmail.com>
2023-09-09 18:04:13 +0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Grid from './Grid';
+import './App.css';
 
 // solve the css, range utility and Grid component 
 
 function App() {
   return (
     <Grid
-      numRows={8}
-      numCols={6}
+      numRows={4}
+      numCols={4}
     />
   );
 }
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,7 +1,21 @@
-import { range } from './utils';
+import { range } from "./utils";
 
 function Grid({ numRows, numCols }) {
-  return <div className="grid">{/* TODO */}</div>;
+const rows = range(numRows);
+const cols = range(numCols)
+
+  return (
+    <div className="grid">
+      {rows.map((row) => (
+        <div key={row} className="grid-row">
+          {cols.map((col) => (
+            <div key={col} className="grid-cell">
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Grid;
